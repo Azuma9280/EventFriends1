5a26a5d252097b5f46d9531561b28a3adadc7afb
Azuma9280 <kuzuya9280@yahoo.co.jp>
2023-09-04 20:45:25 +0900

    コメント作成のルーティング整理＆保存機能見直し

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Thread;
+use App\Models\Eventdate;
 use Illuminate\Http\Request;
 use App\Http\Requests\ThreadRequest;
     
@@ -10,12 +11,24 @@ class ThreadController extends Controller
 {
     public function create(Thread $thread)
     {
-        return view('threads.create')->with(['threads' => $thread->get()]);
+        return view('threads.create'); //->with(['threads' => $thread->get()]);
     }
-    public function store(ThreadRequest $request,Thread $thread)
+    
+    public function show(Thread $Thread)
+    {
+        return view('threads.show')-with(['threads' => $thread]);
+    }
+    
+    public function store(ThreadRequest $request,Thread $thread,Eventdate $event_date)
     {
         $input = $request['thread'];
-        $post->fill($input)->save();
-        return redirect()->route('show');
+        //upload_imageの送信、保存　カリキュラム
+        $thread->fill($input)->save();
+        $input = $request['eventdate'];
+        $input['thread_id'] = $thread->id;
+        $event_date->fill($input)->save();
+        
+        return redirect('/threads/' .$thread->id);
     }
+    
 }
